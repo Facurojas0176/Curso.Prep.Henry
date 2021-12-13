c137cbfe1efdb78fb475f6cb5efd3127f83d630a
unknown <facu.rojasberna@gmail.com>
2021-12-13 17:06:00 -0300

    ejercicio reverse actualizado

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -40,8 +40,12 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse =function(){
-    
+  String.prototype.reverse = function(){
+    var newString = "";
+    for (var i = this.length - 1; i >= 0; i--) {
+        newString += this.charAt(i);
+    }
+    return newString;
   }
 }
 
